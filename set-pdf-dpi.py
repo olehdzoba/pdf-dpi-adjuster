6daea39fef5720b99eb67f6847532d29cf3150f5
Oleh Dzoba <oleh.dzoba.mail@gmail.com>
2023-11-05 12:28:06 +0100

    Improved error handling, renamed main.py to more appropriate filename, added option to set specif DPI

--- set-pdf-dpi.py
+++ set-pdf-dpi.py
@@ -18,6 +18,7 @@ def extract_picture(pdf_path, out_path):
 
     page = pdf_document.load_page(0)
 
+    # !! This way of extracting image does not preserve DPI of the image and always gives 96x96ppi
     pix = page.get_pixmap()
     pix.save(out_path)
 
@@ -37,29 +38,33 @@ def img_to_pdf(img_path, out_path):
 	    f.write(img2pdf.convert(img_path))
 
 
-def fix_pdf(original_pdf):
-    start = time.perf_counter()
-
-    with tempfile.TemporaryDirectory() as tmpdir:
-        # Moving original file to temporary directory while it is being manipulated
-        moved_pdf = f"{tmpdir}/{os.path.basename(original_pdf)}"
-        shutil.move(original_pdf, moved_pdf)
+def fix_pdf(original_pdf, dpi):
+    try:
+        start = time.perf_counter()
         
-        scan_img = f"{tmpdir}/_scan.png"
-        scan_img300 = f"{tmpdir}/_scan300.png"
-    
-        extract_picture(moved_pdf, scan_img)
-        change_dpi(scan_img, scan_img300, (300, 300))
-        img_to_pdf(scan_img300, original_pdf)
+        with tempfile.TemporaryDirectory() as tmpdir:
+            # Moving original file to temporary directory while it is being manipulated
+            moved_pdf = f"{tmpdir}/{os.path.basename(original_pdf)}"
+            shutil.move(original_pdf, moved_pdf)
+            
+            scan_img = f"{tmpdir}/_scan.png"
+            scan_img300 = f"{tmpdir}/_scan300.png"
+        
+            extract_picture(moved_pdf, scan_img)
+            change_dpi(scan_img, scan_img300, dpi)
+            img_to_pdf(scan_img300, original_pdf)
+        
+            end = time.perf_counter()
+            print(f"Fixed {original_pdf}: {round(end - start, 2)}s")
 
-    end = time.perf_counter()
-    print(f"{original_pdf}: {round(end - start, 2)}")
+    except Exception as e:
+        print(f"Skipped file {original_pdf} due to exception: {e}")
 
 
 class Watcher(FileSystemEventHandler):
-    def __init__(self, folder):
+    def __init__(self, dpi):
         super().__init__()
-        self.folder = folder
+        self.dpi = dpi
 
     def on_created(self, event):
         if event.is_directory:
@@ -73,11 +78,11 @@ class Watcher(FileSystemEventHandler):
             # Required so that the watcher does not react to the processed file
             handled_files.append(event.src_path)
 
-            fix_pdf(event.src_path)
+            fix_pdf(event.src_path, self.dpi)
         
 
-def watch_folder(folder):
-    event_handler = Watcher(folder)
+def watch_folder(folder, dpi):
+    event_handler = Watcher(dpi)
     observer = Observer()
 
     observer.schedule(event_handler, folder, recursive=False)
@@ -91,19 +96,22 @@ def watch_folder(folder):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="A CLI tool that sets resolution of the scan within PDF to 300x300ppi.")
+    parser = argparse.ArgumentParser(description="A CLI tool that sets resolution of the image within PDF to custom DPI without changing its demensions. Caution: may distort the size of the image if you process the same image twice or if you set incorrect DPI.")
     subparsers = parser.add_subparsers(title="Commands", dest="command")
 
     fix_parser = subparsers.add_parser("fix", help="Fix resolution of a specific file.")
     fix_parser.add_argument("filename", help="Path to the file to fix.")
+    fix_parser.add_argument("-x", "--dpi", help="DPI value that you want to set for PDF image (e.g 300,300).")
 
     watch_parser = subparsers.add_parser("watch", help="Watch a specific directory and fix all files there.")
     watch_parser.add_argument("dirname", help="Path to the directory to watch.")
+    watch_parser.add_argument("-x", "--dpi", help="DPI value that you want to set for PDF image (e.g 300,300).")
 
     args = parser.parse_args()
-
+    dpi = tuple(map(int, (args.dpi if args.dpi else "300,300").split(',')))
+    
     if args.command == "fix":
-        fix_pdf(args.filename)
+        fix_pdf(args.filename, dpi)
     
     if args.command == "watch":
-        watch_folder(args.dirname)
+        watch_folder(args.dirname, dpi)
